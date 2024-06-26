97e3b8f2af91ce13d2a2e3ca395f480e2f666896
dev766 <akash>
2024-06-27 00:32:43 +0530

    Initial Commit

---
+++ CreateShapes/CreateShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateShapesApp.swift
+//  CreateShapes
+//
+//  Created by Apple on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CreateShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
